8450eae28cb358069338675bcbc1eb5d87b3d827
tojawawel <bgawel93@gmail.com>
2018-10-19 12:07:57 +0200

    Add stock search, ajax and layout

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,17 @@
-class Stock < ApplicationRecord
+class Stock < ActiveRecord::Base
+
+  def self.new_from_lookup(ticker_symbol)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      price = looked_up_stock.latest_price
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
+    rescue Exception => e
+      return nil
+    end
+  end
+
+  def self.strip_commas(number)
+    number.gsub(",","")
+  end
+
 end
