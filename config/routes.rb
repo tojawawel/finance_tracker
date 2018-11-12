2f7f6d9aa0e452bb89ee58ee86cc7161554666c8
tojawawel <bgawel93@gmail.com>
2018-11-12 11:03:27 +0100

    Add frienship model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations"}
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'my_friends', to: "users#my_friends"
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_152333) do
+ActiveRecord::Schema.define(version: 2018_11_12_095012) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
